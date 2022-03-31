02b37f64cc3cc99acbb8bcdd5dcd62377653597e
fernandogtorrez <fernandogtorrez@gmail.com>
2022-03-31 13:07:50 -0500

    start

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use(passport.initialize())
 app.use('/api', Router)
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/buil'))
+    app.use(express.static('client/build'))
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname+'/client/buil/index.html'))
+        res.sendFile(path.join(__dirname+'/client/build/index.html'))
     })
 }
 
